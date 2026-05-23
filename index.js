c61d814cea9f0aa3fa7a5cf7d60291e71124476a
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-05-23 16:04:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ const qrcode = require("qrcode-terminal")
 const chalk = require("chalk")
 const fs = require("fs")
 
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(__dirname))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(3000, () => {
+  console.log("Web panel running on port 3000")
+})
+
 // =======================
 // ⚙️ CONFIG
 // =======================
