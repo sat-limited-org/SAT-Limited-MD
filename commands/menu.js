d79c1b38efffca031dbf87d42a1f35a68608a655
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-06-08 22:09:54 +0200

    Update menu to include all commands in the bot

--- commands/menu.js
+++ commands/menu.js
@@ -7,33 +7,38 @@ module.exports = {
 ╭━━〔 SAT LIMITED MD 〕━━⬣
 ┃ 🤖 WhatsApp Multi Device Bot
 ┃ ⚡ Fast & Stable
+┃ 📱 All Commands Available
 ╰━━━━━━━━━━━━━━⬣
 
 ╭━━〔 MAIN COMMANDS 〕━━⬣
-┃ .menu
-┃ .ping
-┃ .owner
-┃ .alive
+┃ .menu - Show this menu
+┃ .ping - Check bot status
+┃ .owner - Get owner info
+┃ .cmdcount - Total commands count
 ╰━━━━━━━━━━━━━━⬣
 
 ╭━━〔 FUN COMMANDS 〕━━⬣
-┃ .joke
-┃ .quote
-┃ .fact
+┃ .joke - Get a random joke
+┃ .quote - Get a random quote
+┃ .fact - Get a random fact
 ╰━━━━━━━━━━━━━━⬣
 
 ╭━━〔 GROUP COMMANDS 〕━━⬣
-┃ .tagall
-┃ .kick
-┃ .promote
-┃ .demote
+┃ .kick - Kick a user (Admin)
+┃ .promote - Make admin (Admin)
+┃ .demote - Remove admin (Admin)
+┃ .welcome - Welcome message (Admin)
 ╰━━━━━━━━━━━━━━⬣
 
 ╭━━〔 SYSTEM 〕━━⬣
 ┃ Version: 1.0.0
 ┃ Bot: SAT Limited MD
+┃ Prefix: .
 ╰━━━━━━━━━━━━━━⬣
-`
+
+✨ *Use any command with the dot (.) prefix!*
+> For support, type .owner
+`;
 
     await sock.sendMessage(from, {
       text: menuText
